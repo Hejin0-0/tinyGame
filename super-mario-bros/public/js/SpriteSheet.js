db3733f336b3531599c61d30e6da3c2195756916
Hejin0-0 <lovenstein0624@gmail.com>
2024-04-15 14:30:58 +0900

    #02 - Sprites & Layers

--- super-mario-bros/public/js/SpriteSheet.js
+++ super-mario-bros/public/js/SpriteSheet.js
@@ -1,36 +1,37 @@
 export default class SpriteSheet {
-    constructor(image, w = 16, h = 16) {
-        this.image = image;
-        this.width = w;
-        this.height = h;
-        this.tiles = new Map();
-    }
+	constructor(image, width, height) {
+		this.image = image;
+		this.width = width;
+		this.height = height;
+		this.tiles = new Map();
+	}
 
-    define(name, x, y) {
-        const buffer = document.createElement('canvas');
-        buffer.height = this.height;
-        buffer.width = this.width;
-        buffer
-            .getContext('2d')
-            .drawImage(
-                this.image,
-                this.width * x,
-                this.height * y,
-                this.width,
-                this.height,
-                0,
-                0,
-                this.width,
-                this.height);
-        this.tiles.set(name, buffer);
-    }
+	define(name, x, y, width, height) {
+		const buffer = document.createElement("canvas");
+		buffer.width = width;
+		buffer.height = height;
+		buffer
+			.getContext("2d")
+			.drawImage(this.image, x, y, width, height, 0, 0, width, height);
+		this.tiles.set(name, buffer);
+	}
 
-    draw(name, context, x, y) {
-        const buffer = this.tiles.get(name);
-        context.drawImage(buffer, x, y);
-    }
+	defineTile(name, x, y) {
+		this.define(
+			name,
+			x * this.width,
+			y * this.height,
+			this.width,
+			this.height
+		);
+	}
 
-    drawTile(name, context, x, y) {
-        this.draw(name, context, x * this.width, y * this.height);
-    }
+	draw(name, context, x, y) {
+		const buffer = this.tiles.get(name);
+		context.drawImage(buffer, x, y);
+	}
+
+	drawTile(name, context, x, y) {
+		this.draw(name, context, x * this.width, y * this.height);
+	}
 }
